6483809c567c75ae300208257e690ea716c3bc39
Kamrul Hassan <kamrulthedev@gmail.com>
2023-11-23 13:54:41 +0600

    Firbesa-commit

--- src/Compunents/Login/Login.jsx
+++ src/Compunents/Login/Login.jsx
@@ -1,6 +1,17 @@
 import React from 'react';
 
 const Login = () => {
+
+
+    const handlerLoginSubmit = e =>{
+        e.preventDefault();
+        const email = e.target.email.value;
+        const password = e.target.password.value;
+        console.log(email, password)
+
+    }
+
+
     return (
         <div>
         <div className="hero min-h-screen bg-base-200">
@@ -10,18 +21,19 @@ const Login = () => {
       <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
     </div>
     <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-      <form >
-        <div className="form-control">
+      <form className='card-body'>
+      <form  onSubmit={handlerLoginSubmit}>     
+         <div className="form-control">
           <label className="label">
             <span className="label-text">Email</span>
           </label>
-          <input type="email" placeholder="email" className="input input-bordered" required />
+          <input type="email" name='email' placeholder="email" className="input input-bordered" required />
         </div>
         <div className="form-control">
           <label className="label">
             <span className="label-text">Password</span>
           </label>
-          <input type="password" placeholder="password" className="input input-bordered" required />
+          <input type="password" name='password' placeholder="password" className="input input-bordered" required />
           <label className="label">
             <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
           </label>
@@ -29,6 +41,7 @@ const Login = () => {
         <div className="form-control mt-6">
           <button className="btn btn-primary">Login</button>
         </div>
+        </form>
       </form>
     </div>
   </div>
