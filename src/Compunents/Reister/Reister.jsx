88d9c5c37d6c73e26f115775d9c043cb49306e0f
Kamrul Hassan <kamrulthedev@gmail.com>
2023-11-23 12:29:15 +0600

    router-commit

--- src/Compunents/Reister/Reister.jsx
+++ src/Compunents/Reister/Reister.jsx
@@ -4,18 +4,33 @@ import { createUserWithEmailAndPassword } from 'firebase/auth';
 
 const Reister = () => {
   const [registerError, setRegisterError] = useState('')
+  const [sucsses, setSucsses] = useState('')
 
   const handleRegister = e =>{
     e.preventDefault();
     const email = e.target.email.value;
     const password = e.target.password.value;
     console.log(email, password)
+
+       // reset error
+       setRegisterError('');
+       setSucsses('');
+       
+    //error massegs
+    if(password.length < 6){
+      setRegisterError('Password should be at least 6 characters is longer ')
+      return;
+    } 
+
+ 
     /*
     create user
     */ 
      createUserWithEmailAndPassword(auth, email, password)
     .then(result =>{
       console.log(result.user);
+      setSucsses('your register is sucsses')
+
     })
     .catch(error =>{
      console.log(error);
@@ -32,12 +47,15 @@ const Reister = () => {
             <form onSubmit={handleRegister} >
                 <input className='border-2 rounded-lg mt-4 p-2 w-80' type="email" placeholder='Your Email' name="email" id="" />
                 <br />
-                <input  type="password" name="password" placeholder='Password' id="" className="mt-4 border-2  rounded-lg p-2 w-80" />
+                <input  type="password" name="password" placeholder='Password' required id="" className="mt-4 border-2  rounded-lg p-2 w-80" />
                 <br />
-             <input type="submit"className="mt-4 btn btn-primary border-2  rounded-lg p-2 w-80" value="REGISTER" id="" />
+             <input type="submit"className="mt-4 btn btn-primary border-2  rounded-lg p-2 w-80" required value="REGISTER" id="" />
             </form>
            {
             registerError && <p className='text-red-500 mt-6'>{registerError}</p>
+           }
+           {
+            sucsses && <p className='text-green-600 mt-6'>{sucsses}</p>
            }
           </div>
         </div>
